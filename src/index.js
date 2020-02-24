91561786d488c0faea2d996b5693ea43ebf7d0ff
SajaSwalgah <saja.swalgah@yahoo.com>
2020-02-25 01:50:02 +0200

    update package.json in order to deploy in netlify

--- src/index.js
+++ src/index.js
@@ -4,3 +4,8 @@ import ReactDOM from 'react-dom';
 import App from './app.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// https://github.com/Saja-401-advanced-javascript/class-27/actions?query=workflow%3A%22Run+Javascript+Tests%22
+//https://github.com/Saja-401-advanced-javascript/class-27/pull/1
+//09bd85081fece2074bef0453363327f06afc9e06
+//https://github.com/Saja-401-advanced-javascript/class-27/actions/runs/44579894
